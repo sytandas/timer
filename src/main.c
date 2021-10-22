9ad62e50e69f070a579d4ec1ebff81a2be014618
sayantan3 <sayantand975@gmail.com>
2021-10-22 19:42:28 +0530

    update

--- src/main.c
+++ src/main.c
@@ -1,7 +1,13 @@
+/*
+ * timer - text based stopwatch and countdown timer tool incorporated into terminal
+ * This program is under MIT license 
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <signal.h>
 
-
+int main(int argc, char* argv[]){
+  
+}
 
